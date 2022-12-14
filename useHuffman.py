355b59a33c82cee7ce8d01ddd2c484e164438bf8
Athena <tersitabm@gmail.com>
2022-12-13 19:30:43 -0500

    added exec time

--- useHuffman.py
+++ useHuffman.py
@@ -1,12 +1,13 @@
 from huffman import HuffmanCoding
 import sys
+import time 
 
 path = "sample.txt"
+time_start = time.time()
 
 h = HuffmanCoding(path)
-
 output_path = h.compress()
-print("Compressed file path: " + output_path)
-
 decom_path = h.decompress(output_path)
-print("Decompressed file path: " + decom_path)
+
+time_end = time.time()
+print(time_end-time_start)
